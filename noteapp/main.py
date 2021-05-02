20733feab52820b89b372af948e3f73eda281517
Demi Jon <pepino.adamek@gmail.com>
2021-05-03 01:48:36 +0200

    Implemented tags

--- noteapp/main.py
+++ noteapp/main.py
@@ -27,47 +27,162 @@ async def database_disconnect():
 
 # Create table
 async def create_table():
-    query = '''CREATE TABLE IF NOT EXISTS note_table (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        content TEXT NOT NULL)'''
-    await database.execute(query=query)
+    query1 = '''CREATE TABLE IF NOT EXISTS 
+        note_table (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            created INTEGER,
+            changed INTEGER,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL
+        )'''
+    query2 = '''CREATE TABLE IF NOT EXISTS 
+        tag_table (
+            tag_id INTEGER PRIMARY KEY,
+            tag_name TEXT NOT NULL
+        )'''
+    query3 = '''CREATE TABLE IF NOT EXISTS 
+        note_tag_mapping (
+            note_reference         INTEGER,
+            tag_reference          INTEGER
+        )'''
+    await database.execute(query=query1)
+    await database.execute(query=query2)
+    await database.execute(query=query3)
+
+### Tags #######################################################################
+
+def is_in_list(my_list, string):
+    for x in my_list:
+        if string.lower().strip() == x.lower().strip():
+            return True
+    return False
+
+async def inspect_tags():
+    query3 = '''SELECT note_table.id, group_concat(tag_name,',')
+        FROM note_tag_mapping
+        JOIN note_table ON note_reference = note_table.id
+        JOIN tag_table ON tag_reference = tag_table.tag_id
+        GROUP BY note_table.id'''
+    rows = await database.fetch_all(query=query3)
+    for x in rows:
+        print(x)
+
+async def add_tag_to_note(note_id, tag_name):
+    # Don't accept empty string as tag
+    if tag_name == "":
+        return
+
+    # Find out if tag exists already
+    query1 = f'SELECT EXISTS (SELECT 1 FROM tag_table WHERE tag_name = "{tag_name}")'
+    result = await database.fetch_val(query=query1)
+    
+    if result == 0:
+        # Tag doesn't exist yet, create it
+        query2 = f'INSERT INTO tag_table (tag_name) VALUES ("{tag_name}")'
+        await database.execute(query=query2)
+    
+    # Get tag id
+    query3 = f'SELECT tag_id FROM tag_table WHERE tag_name = "{tag_name}"'
+    tag_id = await database.fetch_val(query=query3)
+
+    # Insert note-tag mapping only if it doesn't exist yet
+    query4 = f'''INSERT INTO note_tag_mapping (note_reference, tag_reference)
+        SELECT {note_id}, {tag_id} 
+        WHERE NOT EXISTS(SELECT 1 FROM note_tag_mapping 
+        WHERE note_reference = {note_id} AND tag_reference = {tag_id})'''
+    await database.execute(query=query4)
+
+async def remove_tag_from_note(note_id, tag_name):
+    # Get tag id
+    query1 = f'SELECT tag_id FROM tag_table WHERE tag_name = "{tag_name}"'
+    tag_id = await database.fetch_val(query=query1)
+
+    # Remove note-tag connection in db
+    query2 = f"DELETE FROM note_tag_mapping WHERE tag_reference={tag_id} AND note_reference = {note_id}"
+    await database.execute(query=query2)
+
+async def get_note_tags(note_id):
+    # Get note tags from database
+    query = f'''SELECT group_concat(tag_name,',')
+        FROM note_tag_mapping
+        JOIN note_table ON note_reference = note_table.id
+        JOIN tag_table ON tag_reference = tag_table.tag_id
+        WHERE note_table.id = {note_id}'''
+    tags_str = await database.fetch_val(query=query)
+
+    # Return tags in list
+    if tags_str is None:
+        return []
+    else:
+        return tags_str.split(",")
+
+async def update_note_tags(note_id, frontend_tags):
+    backend_tags = await get_note_tags(note_id)
+    
+    # Compare frontend and backend tags
+    new_tags = [tag for tag in frontend_tags if not is_in_list(backend_tags, tag)]
+    removed_tags = [tag for tag in backend_tags if not is_in_list(frontend_tags, tag)]
+
+    # Add new tags
+    for tag in new_tags:
+        await add_tag_to_note(note_id, tag)
+
+    # Remove missing tags
+    for tag in removed_tags:
+        await remove_tag_from_note(note_id, tag)
 
 ### Handlers ###################################################################
 
 async def create_note(item):
     # Save note to database
-    query = "INSERT INTO note_table (title, content) VALUES (:title, :content)"
+    query1 = "INSERT INTO note_table (created, changed, title, content) VALUES (:created, :changed, :title, :content)"
     values = {
+        "created": item['created'], 
+        "changed": item['changed'], 
         "title": item['title'], 
         "content": item['content']}
-    await database.execute(query=query, values=values)
+    await database.execute(query=query1, values=values)
 
     # Get new note id
     query2 = "SELECT * FROM note_table ORDER BY id DESC LIMIT 1"
-    result = await database.fetch_one(query=query2)
-    note_id = result[0]
+    note_id = await database.fetch_val(query=query2)
+
+    # Update tags of note
+    await update_note_tags(note_id, item['tags'])
 
     # Send note back to frontend with correct ID
     return {
         'method': 'POST_BACK',
         'id': note_id,
+        'created': item['created'], 
+        'changed': item['changed'], 
         'title': item['title'],
-        'content': item['content']
+        'content': item['content'],
+        'tags': item['tags']
     }
 
 async def read_notes():
-    # Run a database query
-    query = "SELECT * FROM note_table ORDER BY id DESC LIMIT 10"
+    # Get notes with tags
+    query = '''SELECT note_table.id, created, changed, title, content, group_concat(tag_name,',')
+        FROM note_table
+        LEFT JOIN note_tag_mapping ON note_table.id = note_tag_mapping.note_reference
+        LEFT JOIN tag_table ON note_tag_mapping.tag_reference = tag_table.tag_id
+        GROUP BY note_table.id
+        ORDER BY note_table.changed DESC LIMIT 10'''
     rows = await database.fetch_all(query=query)
 
     # Put notes in list of dicts for frontend 
     note_list = []
     for note in rows:
+        # Split tags from database by comma and create taglist
+        tag_list = note[5].split(",") if note[5] is not None else []
         note_list.append({
             'id': note[0],
-            'title': note[1],
-            'content': note[2]
+            'created': note[1],
+            'changed': note[2],
+            'title': note[3],
+            'content': note[4],
+            'tags': tag_list
         })
     
     return {
@@ -76,12 +191,16 @@ async def read_notes():
     }
 
 async def edit_note(item):
-    query = "UPDATE note_table SET title=:title, content=:content WHERE id=:id"
+    query = "UPDATE note_table SET changed=:changed, title=:title, content=:content WHERE id=:id"
     values = {
         "id": item['id'], 
+        "changed": item['changed'], 
         "title": item['title'], 
         "content": item['content']}
     await database.execute(query=query, values=values)
+
+    # Update tags of note
+    await update_note_tags(item['id'], item['tags'])
 
     return {'method': 'EDIT_BACK'}
 
@@ -91,6 +210,35 @@ async def delete_note(item):
     await database.execute(query=query, values=values)
 
     return {'method': 'DELETE_BACK'}
+
+async def search_notes(item):
+    text = item['text']
+    query = f'''SELECT note_table.id, created, changed, title, content, group_concat(tag_name,',')
+        FROM note_table
+        LEFT JOIN note_tag_mapping ON note_table.id = note_tag_mapping.note_reference
+        LEFT JOIN tag_table ON note_tag_mapping.tag_reference = tag_table.tag_id
+        WHERE title LIKE "%{text}%" OR content LIKE "%{text}%"
+        GROUP BY note_table.id
+        ORDER BY note_table.changed DESC LIMIT 10'''
+    rows = await database.fetch_all(query=query)
+
+    # Only save notes containing search text
+    note_list = []
+    for note in rows:
+        tag_list = note[5].split(",") if note[5] is not None else []
+        note_list.append({
+            'id': note[0],
+            'created': note[1],
+            'changed': note[2],
+            'title': note[3],
+            'content': note[4],
+            'tags': tag_list
+        })
+    
+    return {
+        'method': 'SEARCH_BACK',
+        'note_list': note_list
+    }
 
 ### Routes #####################################################################
 
@@ -105,6 +253,8 @@ async def read_index():
 async def websocket_endpoint(websocket: WebSocket):
     # Wait for frontend to accept connection
     await websocket.accept()
+
+    # await inspect_tags()
 
     try:
         while True:
@@ -130,6 +280,10 @@ async def websocket_endpoint(websocket: WebSocket):
             elif item['method'] == 'DELETE':
                 # Inform frontend that note has been removed on server
                 message = await delete_note(item)
+                await websocket.send_json(message)
+            
+            elif item['method'] == 'SEARCH':
+                message = await search_notes(item)
                 await websocket.send_json(message)
                     
     except WebSocketDisconnect:
--- noteapp/static/script.js
+++ noteapp/static/script.js
@@ -1,13 +1,16 @@
 /* Manage reconnection of websockets. Everytime connection is lost, 
 functions must be assigned again to the new websocket */
 var ws;
+var firstFetch = true;
 function connect() {
     ws = new WebSocket('ws://localhost:8000/ws');
     ws.onopen = function () {
         // Get first few notes (Do I need note refresh every reconnection?)
-        ws.send(JSON.stringify({
-            'method': 'GET'
-        }));
+        if (firstFetch) {
+            ws.send(JSON.stringify({
+                'method': 'GET'
+            }));
+        }
     };
 
     // Wait for messages from server
@@ -17,8 +20,8 @@ function connect() {
         // Decide what to do with message from server
         if (noteObj['method'] == 'GET_BACK') {
             var noteList = noteObj['note_list']
-            for (var item in noteList) {
-                displayNote(noteList[item], false)
+            for (var item of noteList) {
+                displayNote(item, false)
             }
         } 
         else if (noteObj['method'] == 'POST_BACK') {
@@ -31,10 +34,20 @@ function connect() {
         else if (noteObj['method'] == 'EDIT_BACK') {
             notify('EDITED', 'green')
         }
+        else if (noteObj['method'] == 'SEARCH_BACK') {
+            removeNotes();
+
+            // Display notes displaying searched text
+            var noteList = noteObj['note_list']
+            for (var item of noteList) {
+                displayNote(item, false)
+            }
+        }
     };
 
     // Try to reconnect (in case of server reload etc.)
     ws.onclose = function (event) {
+        firstFetch = false;
         console.log('Socket is closed. Reconnect will be attempted in 1 second.', event.reason);
         setTimeout(function () {
             connect();
@@ -49,13 +62,15 @@ function connect() {
 }
 connect();
 
+/// Helper functions ///////////////////////////////////////////////////////////
+
 // Visualy comfort user, that his data is saved on server
 function notify(text, color){
     var res = document.querySelector("#result");
     var orig_color = res.style.color;
     var orig_text = res.textContent;
 
-    // Don't change if it's in process of changing
+    // Don't change result if it's in process of changing
     if (orig_text == 'RESULT') {
         res.style.color = color;
         res.textContent = text;
@@ -66,34 +81,60 @@ function notify(text, color){
     }
 }
 
+function timestampToDatetime(timestamp) {
+    var datetime = new Date(timestamp);
+    return datetime.toLocaleDateString("cs-CZ") + ' - ' +  datetime.toLocaleTimeString("cs-CZ")
+}
+
+function tagStrToList(tagStr) {
+    var tagList = [];
+    for (var tag of tagStr.split(',')) {
+        if (tag != "") {
+            tagList.push(tag.trim());
+        }
+    }
+    return tagList
+}
+
 /// CRUD functions /////////////////////////////////////////////////////////////
 
 function submitNote(event) {
     var title = document.querySelector("#noteTitleSubmit")
     var content = document.querySelector("#noteContentSubmit")
+    var tags = document.querySelector("#noteTagsSubmit")
     
     // Send message to server with note data
+    var timestamp = Math.floor(Date.now());
     var obj = {
         'method': 'POST',
+        'created': timestamp,
+        'changed': timestamp,
         'title': title.value,
-        'content': content.value
+        'content': content.value,
+        'tags': tagStrToList(tags.value)
     }
     ws.send(JSON.stringify(obj))
     
     // Clear textareas
     title.value = ''
     content.value = ''
+    tags.value = ''
 }
 
 function displayNote(noteObj, prepend) {
     var notesContainer = document.querySelector(".notesContainer");
     var noteTemplate = document.querySelector('#noteTemplate');
-
+    
     // Fill template of note with data
     var note = noteTemplate.content.cloneNode(true);
     note.querySelector(".note").dataset.noteid = noteObj['id']; // Use data attribute for id
+    note.querySelector(".noteChanged").textContent = timestampToDatetime(noteObj['changed'])
     note.querySelector(".noteTitle").textContent = noteObj['title'];
     note.querySelector(".noteContent").textContent = noteObj['content'];
+    
+    // Display tags
+    var tagsContainer = note.querySelector(".noteTags");
+    displayTags(tagsContainer, noteObj['tags']);
 
     // Decide if prepend or append note
     if (prepend) {
@@ -103,6 +144,25 @@ function displayNote(noteObj, prepend) {
     }
 }
 
+function displayTags(tagsContainer, tagList) {
+    var tagTemplate = document.querySelector('#tagTemplate');
+    tagsContainer.dataset.taglist = tagList; // Use data attribute for saving tags
+    for (var tagName of tagList) {
+        var element = tagTemplate.content.cloneNode(true);
+        element.querySelector(".tag").textContent = tagName;
+        tagsContainer.append(element);
+    }
+}
+
+function removeNotes() {
+    // Remove notes currently on canvas
+    var notesContainer = document.querySelector(".notesContainer");
+    while (notesContainer.firstChild) {
+        notesContainer.firstChild.remove()
+    }
+}
+
+// Change appearance of note while editing
 function editNote(event) {
     var innerNoteElement = event.currentTarget.parentNode.parentNode;
     var noteElement = innerNoteElement.parentNode;
@@ -115,39 +175,49 @@ function editNote(event) {
     var noteEdit = noteEditTemplate.content.cloneNode(true);
     noteEdit.querySelector("#noteTitleEdit").value = innerNoteElement.querySelector(".noteTitle").textContent;
     noteEdit.querySelector("#noteContentEdit").value = innerNoteElement.querySelector(".noteContent").textContent;
+    noteEdit.querySelector("#noteTagsEdit").value = innerNoteElement.querySelector(".noteTags").dataset.taglist;
     noteElement.append(noteEdit)
 }
 
+// Update note content on backend
 function updateNote(event) {
     var innerNoteEditElement = event.currentTarget.parentNode.parentNode;
     var noteElement = innerNoteEditElement.parentNode;
     var innerNoteElement = noteElement.querySelector(".innerNote");
     var old_title = innerNoteElement.querySelector(".noteTitle").textContent;
     var old_content = innerNoteElement.querySelector(".noteContent").textContent;
+    var old_tags = innerNoteElement.querySelector(".noteTags").dataset.taglist;
     
     // Get new data and remove element for editing
     var title = document.querySelector("#noteTitleEdit").value;
     var content = document.querySelector("#noteContentEdit").value;
+    var tags = document.querySelector("#noteTagsEdit").value;
     innerNoteEditElement.remove();
     
     // In case that strings didn't change, skip database change
-    if (title.trim() == old_title.trim() && content.trim() == old_content.trim()) {
+    if (title.trim() == old_title.trim() && 
+        content.trim() == old_content.trim() &&
+        tags.trim() == old_tags.trim()) {
         // Unhide note
         innerNoteElement.style.display = '';
     } else {
-        // Fill note with current data and unhide it
-        innerNoteElement.querySelector(".noteTitle").textContent = title;
-        innerNoteElement.querySelector(".noteContent").textContent = content;
-        innerNoteElement.style.display = '';
-        
-        // Send edited note to server
-        var obj = {
+        // Remove hidden old note 
+        noteElement.remove();
+
+        // Create note object filled with new data
+        var timestamp = Math.floor(Date.now());
+        var noteObj = {
             'method': 'EDIT',
             'id': noteElement.dataset.noteid,
+            'changed': timestamp,
             'title': title,
-            'content': content
+            'content': content,
+            'tags': tagStrToList(tags)
         }
-        ws.send(JSON.stringify(obj))
+
+        // Display edited note and send it to server
+        displayNote(noteObj, true)
+        ws.send(JSON.stringify(noteObj))
     }
 }
 
@@ -163,4 +233,18 @@ function deleteNote(event) {
 
     // Remove note from HTML canvas
     noteElement.remove()
+}
+
+function searchText(event) {
+    if (event.key == "Enter") {
+        // Send message to server with id of note to be deleted
+        var inputArea = event.currentTarget;
+        var obj = {
+            'method': 'SEARCH',
+            'text': inputArea.value
+        }
+        ws.send(JSON.stringify(obj))
+
+        inputArea.value = ''
+    }
 }
